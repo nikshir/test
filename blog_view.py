c3fa19c50e17dca99f78b90ca1be12da59f9f159
Andrei Nikolin <127049433+nikshir@users.noreply.github.com>
2023-04-04 02:06:33 +0300

    Update blog_view.py

--- blog_view.py
+++ blog_view.py
@@ -1,8 +1,12 @@
-posts_base = {post['id']:post for post in posts}
+ERROR_404_POST_DETAIL = (
+    'Публикация с id:"{post_id}" отсутствует на сервере.'
+)
+posts_base = {post['id']: post for post in posts}
+
 
 def post_detail(request, post_id):
     template = 'blog/detail.html'
     if post_id not in posts_base.keys():
         raise Http404(ERROR_404_POST_DETAIL.format(post_id=post_id))
-    context = {'post': posts_base['post_id']}
+    context = {'post': posts_base[post_id]}
     return render(request, template, context)
